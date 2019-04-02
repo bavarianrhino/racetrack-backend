88bc64755e4ff62d060b71ca0d214e77251eef04
github username <github email address>
2019-04-02 17:52:59 -0400

    added racecar controller

--- app/controllers/api/v1/racecar_controller.rb
+++ app/controllers/api/v1/racecar_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::RacecarController < ApplicationController
+
+  def index
+		@racecars = Racecar.all
+		render json: @racecars, status: :ok
+	end
+
+	def create
+		@racecar = Racecar.new(racecars_params)
+        if @racecar.save
+            render json: @racecar
+			# render json: @racecar, status: :created
+        else
+            render json: { error: 'failed to create racecar....Ryans Error' }
+			# render json: @racecar.errors.full_messages, status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def racecars_params
+		params.require(:racecar).permit(:name, :up, :down, :left, :right)
+    end
+    
 end
